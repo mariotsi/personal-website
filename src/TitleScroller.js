a19d3bc66aa43cb98316d806892c28a377f94cad
Simone Mariotti <simone@mariotti.me>
2023-08-25 18:15:00 +0200

    Adapt CSS with box-sizing, make the GitHub icon white, fix TitleScroller

--- src/TitleScroller.js
+++ src/TitleScroller.js
@@ -3,17 +3,22 @@ import { useEffect, useRef } from 'react';
 function TitleScroller({ text: fullText }) {
 
   const intervalHandle = useRef()
-  const pageTitle = useRef()
+  const pageTitle = useRef(document.title)
   useEffect(() => {
 
     window.addEventListener('blur', () => {
-      pageTitle.current = document.title
       let text = fullText
 
       intervalHandle.current = setInterval(() => {
         document.title = text
-
-        text = text.substring(3) || fullText
+        if (/^\S{0,3} +$/.test(text)) {
+          // there is no more any visible text
+          text = fullText;
+        } else {
+          // Use U+00A0 instead of normal space due to Safari ignoring the trailing spaces
+          text = text.substring(3) + ' '.repeat(3)
+        }
+        document.title = text
       }, 500)
     })
 
